3906b398124828cbfe1653dc7050deccff070757
j-piar <juraj2.piar@live.uwe.ac.uk>
2015-06-09 10:50:38 +0100

    Finished: Basic plotting  ToDo: Lot more

---
+++ ECG Plotter/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace ECG_Plotter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox_filteredData = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_time = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_rawData = new System.Windows.Forms.TextBox();
+            this.display = new GraphLib.PlotterDisplayEx();
+            this.openFileDialogIn = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1.SuspendLayout();
+            this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripSeparator1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(837, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(38, 22);
+            this.toolStripDropDownButton1.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.mainPanel.Controls.Add(this.splitContainer1);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 25);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(837, 438);
+            this.mainPanel.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox_filteredData);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox_time);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox_rawData);
+            this.splitContainer1.Panel1.Enabled = false;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.display);
+            this.splitContainer1.Size = new System.Drawing.Size(837, 438);
+            this.splitContainer1.SplitterDistance = 279;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // textBox_filteredData
+            // 
+            this.textBox_filteredData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_filteredData.Location = new System.Drawing.Point(103, 20);
+            this.textBox_filteredData.Multiline = true;
+            this.textBox_filteredData.Name = "textBox_filteredData";
+            this.textBox_filteredData.Size = new System.Drawing.Size(104, 418);
+            this.textBox_filteredData.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Time";
+            // 
+            // textBox_time
+            // 
+            this.textBox_time.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_time.Location = new System.Drawing.Point(213, 20);
+            this.textBox_time.Multiline = true;
+            this.textBox_time.Name = "textBox_time";
+            this.textBox_time.Size = new System.Drawing.Size(63, 418);
+            this.textBox_time.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RawData";
+            // 
+            // textBox_rawData
+            // 
+            this.textBox_rawData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_rawData.Location = new System.Drawing.Point(0, 20);
+            this.textBox_rawData.Multiline = true;
+            this.textBox_rawData.Name = "textBox_rawData";
+            this.textBox_rawData.ReadOnly = true;
+            this.textBox_rawData.Size = new System.Drawing.Size(104, 418);
+            this.textBox_rawData.TabIndex = 0;
+            // 
+            // display
+            // 
+            this.display.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.display.BackColor = System.Drawing.Color.Transparent;
+            this.display.BackgroundColorBot = System.Drawing.Color.White;
+            this.display.BackgroundColorTop = System.Drawing.Color.White;
+            this.display.DashedGridColor = System.Drawing.Color.DarkGray;
+            this.display.DoubleBuffering = false;
+            this.display.Location = new System.Drawing.Point(-1, 0);
+            this.display.Name = "display";
+            this.display.PlaySpeed = 0.5F;
+            this.display.Size = new System.Drawing.Size(555, 438);
+            this.display.SolidGridColor = System.Drawing.Color.DarkGray;
+            this.display.TabIndex = 0;
+            // 
+            // openFileDialogIn
+            // 
+            this.openFileDialogIn.FileName = "openFileDialogIn";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 463);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "MainForm";
+            this.Text = "ECG Plotter";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.mainPanel.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialogIn;
+        private System.Windows.Forms.TextBox textBox_rawData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_time;
+        private System.Windows.Forms.TextBox textBox_filteredData;
+        private GraphLib.PlotterDisplayEx display;
+    }
+}
+
---
+++ ECG Plotter/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Linq;
+using System.Windows.Forms;
+using GraphLib;
+using System.Threading;
+using System.Drawing;
+
+namespace ECG_Plotter
+{
+    public partial class MainForm : Form
+    {
+        private XmlDocument doc = null;
+        private object pad_lock = new object();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            loadXML();
+            try
+            {
+                if (!doc.Equals(null))
+                {
+                    processData();
+                }
+            }
+            catch (NullReferenceException nullE)
+            {
+                MessageBox.Show("No file selected!");
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Loading XML file using openFileDialog
+        /// </summary>
+        private void loadXML()
+        {
+            try
+            {
+                openFileDialogIn.CheckFileExists = true;
+                DialogResult openDialog = openFileDialogIn.ShowDialog();
+
+                if (openDialog == DialogResult.OK)
+                {
+
+                    string path = openFileDialogIn.FileName; // The Path to the .Xml file
+                    FileStream fileReader = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite); //Set up the filestream (fileReader)
+                    doc = new XmlDocument();
+                    doc.Load(fileReader); //Load the data from the file into the XmlDocument
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(String.Format("An error occured. Original message: {0}", e.Message));
+            }
+        }
+
+        /// <summary>
+        /// Setup PlotterGraph and extract data from the XML file using separate threads for performance [O(n/2)]
+        /// </summary>
+        private void processData()
+        {
+            this.SuspendLayout();
+            display.DataSources.Clear();
+            display.SetDisplayRangeX(0, 400);
+            display.SetGridDistanceX(50);
+            display.SetGridOriginX(500);
+            display.Smoothing = System.Drawing.Drawing2D.SmoothingMode.None;
+            display.PanelLayout = PlotterGraphPaneEx.LayoutMode.STACKED;
+            
+            display.BackgroundColorBot = Color.White;
+            display.BackgroundColorTop = Color.FromArgb(183, 183, 255);
+            display.SolidGridColor = Color.Blue;
+            display.DashedGridColor = Color.Blue;
+            display.ShowMovingGrid = false;
+
+            new Thread(() => calcData(PointDataType.RawData)).Start();
+            //Thread.Sleep(100);
+            new Thread(() => calcData(PointDataType.FilteredData)).Start();
+
+            this.ResumeLayout();
+            display.Refresh();
+        }
+
+        private Color colourChooser(int i)
+        {
+            Color[] colours = 
+            {
+                Color.DarkCyan,
+                Color.DeepPink,
+            };
+            return colours[i];
+        }
+
+        private void calcData(PointDataType type)
+        {
+            DataSource ds;
+            lock (pad_lock)
+            {
+                display.DataSources.Add(new DataSource());
+            
+                 ds = display.DataSources[(int)type];
+                ds.Name = type.ToString();
+                ds.GraphColor = colourChooser((int)type);
+                ds.OnRenderXAxisLabel += RenderXLabel;
+                ds.AutoScaleY = false;
+                ds.SetDisplayRangeY(-2, 2);
+                ds.SetGridOriginY(0);
+                ds.SetGridDistanceY(1);
+                ds.OnRenderYAxisLabel += RenderYLabel;
+                ds.Length = 500000;
+            }
+            XmlNodeList fData = doc.GetElementsByTagName(type.ToString());
+            int i = 0;
+            foreach (XmlNode node_dataType in fData)
+            {
+                foreach (XmlNode node_dataPoint in node_dataType.ChildNodes)
+                {
+                    if (node_dataPoint.Name == "DataPoint")
+                    {
+                        ds.Samples[i].x = float.Parse(node_dataPoint.Attributes[0].Value) * 100;
+                        ds.Samples[i].y = float.Parse(node_dataPoint.Attributes[1].Value) * 100;
+                        i++;
+                    }
+                }
+            }
+        }
+        private enum PointDataType
+        {
+            RawData = 0,
+            FilteredData = 1
+        }
+        private String RenderXLabel(DataSource s, int idx)
+        {
+            if (s.AutoScaleX)
+            {
+                int Value = (int)(s.Samples[idx].x);
+                return "" + Value;
+            }
+            else
+            {
+                int Value = (int)(s.Samples[idx].x);
+                String Label = "" + Value/100 + "\"";
+                return Label;
+            }
+        }
+
+        private String RenderYLabel(DataSource s, float value)
+        {
+            return String.Format("{0:0.0}", value);
+        }
+    }
+}
