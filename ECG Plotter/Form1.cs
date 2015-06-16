647c750acf2d79caeb12e0a71c631c8ceb09ded9
j-piar <juraj2.piar@live.uwe.ac.uk>
2015-06-16 11:51:47 +0100

    added copyright notice

--- ECG Plotter/Form1.cs
+++ ECG Plotter/Form1.cs
@@ -8,9 +8,37 @@ using System.Windows.Forms;
 using GraphLib;
 using System.Drawing;
 
-
+#region Copyright (©) 2015 Juraj Piar
+/**
+ * Copyright (©) 2015 Juraj Piar (juraj.piar@hotmail.com)
+ * http://github.com/j-piar/ECG-Plotter
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ * THE SOFTWARE.
+ *
+ **/
+#endregion
 namespace ECG_Plotter
 {
+    /// <summary>
+    /// Simple plotting of ECG signals from an XML file.
+    /// <remarks>Using "A simple C# library for graph plotting" library(http://goo.gl/nYsWTZ)</remarks>
+    /// </summary>
     public partial class MainForm : Form
     {
         #region MEMBERS
